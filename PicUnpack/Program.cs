5cfe3d5b72e745882647f5d298dbf8991ad69cd5
Maxim Kovalenko <ardash1100@gmail.com>
2018-06-15 21:38:47 +0300

    Добавлены распаковщик/упаковщик картинок

---
+++ PicUnpack/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace PicUnpack {
+
+  internal class Program {
+
+    private static ushort BBToShort(byte byteL, byte byteH) {
+      return (ushort)((byteH << 8) + byteL);
+    }
+
+    private static ushort BBToShort(byte[] bArr, int offset) {
+      return BBToShort(bArr[offset], bArr[offset + 1]);
+    }
+
+    private static byte getBitPairFromByte(byte inByte, int n) {
+      return (byte)((inByte & 0x03 << n * 2) >> n * 2);
+    }
+
+    private static byte getColor(byte inByte) {
+      const byte red = 0x01;
+      const byte green = 0x02;
+      const byte black = 0x00;
+      const byte white = 0x0F;
+
+      switch (inByte) {
+        case 0x00: return black;
+        case 0x01: return green;
+        case 0x02: return red;
+        case 0x03:
+        default:
+          break;
+      }
+      return white;
+    }
+
+    private static void Main(string[] args) {
+      if (args.Length == 0) return;
+
+      for (int i = 0; i < 10; i++) {
+        byte[] bb = File.ReadAllBytes(args[0] + i.ToString());
+        ushort arrPicL = BBToShort(bb, 0);
+
+        List<byte> bbRes = new List<byte>();
+        for (ushort N = 2; N < arrPicL + 2;) {
+          if (bb[N] == 0x7B) {
+            bb[N + 1]++;
+            for (; bb[N + 1] > 0; bb[N + 1]--) bbRes.Add(bb[N + 2]);
+            N += 3;
+          } else
+            bbRes.Add(bb[N++]);
+        }
+        //File.WriteAllBytes(args[0] + i.ToString() + ".pic", bbRes.ToArray());
+
+        const int imgW = 80;
+        const int imgH = 200;
+
+        // Транспонируем матрицу
+        byte[] bbResArr = new byte[bbRes.Count];
+        for (int x = 0; x < imgW; x++)
+          for (int y = 0; y < imgH; y++)
+            bbResArr[y * imgW + x] = bbRes[x * imgH + y];
+        //File.WriteAllBytes(args[0] + i.ToString() + ".pic2", bbResArr);
+
+        // Преобразуем 2 бита/пиксель в 8 бит/пиксель
+        int bbResArrL = bbResArr.Length;
+
+        List<byte> bbBmpList = new List<byte>();
+        for (int j = 0; j < bbResArrL; j++)
+          for (int k = 3; k >= 0; k--)
+            bbBmpList.Add(getColor(getBitPairFromByte(bbResArr[j], k)));
+
+        // Сохраним в bmp
+        byte[] bbBmp = bbBmpList.ToArray();
+        int bbBmpL = bbBmp.Length;
+
+        Bitmap bmp = new Bitmap(imgW * 4, imgH, PixelFormat.Format8bppIndexed);
+        BitmapData bmpData = bmp.LockBits(new Rectangle(Point.Empty, bmp.Size), ImageLockMode.ReadWrite, bmp.PixelFormat);
+        Marshal.Copy(bbBmp, 0, bmpData.Scan0, bbBmpL);
+        bmp.UnlockBits(bmpData);
+        bmp.Save(args[0] + i.ToString() + ".bmp", ImageFormat.Bmp);
+      }
+    }
+  }
+}
---
+++ picPack/Program.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace picPack {
+
+  internal class Program {
+
+    private enum bColor {
+      bBlack = 0x00,
+      bRed,
+      bGreen,
+      bWhite
+    }
+
+    private static byte cv8bppTo2bpp(byte b, int pos) {
+      byte retB = 0x00;
+      switch (b % 8) {
+        case 0x00: retB = (byte)bColor.bBlack; break;
+        case 0x01: retB = (byte)bColor.bGreen; break;
+        case 0x02: retB = (byte)bColor.bRed; break;
+        case 0x07: retB = (byte)bColor.bWhite; break;
+        default:
+          break;
+      }
+
+      return (byte)(retB << pos * 2);
+    }
+
+    private static void Main(string[] args) {
+      const string fPrefix = "screen";
+      for (int fileN = 0; fileN < 10; fileN++) {
+        Bitmap bmp = new Bitmap(fPrefix + fileN.ToString() + "_.bmp");
+
+        // Считаем, что PixelFormat 8 бит/пиксель
+        int bmp8bppL = bmp.Size.Width * bmp.Size.Height;
+        byte[] bbBmp8bpp = new byte[bmp8bppL];
+
+        BitmapData bmpData = bmp.LockBits(new Rectangle(Point.Empty, bmp.Size), ImageLockMode.ReadOnly, bmp.PixelFormat);
+        Marshal.Copy(bmpData.Scan0, bbBmp8bpp, 0, bmp8bppL);
+        bmp.UnlockBits(bmpData);
+
+        // Преобразовать 8 бит/пиксель в 2 бит/пиксель
+        int bmp2bppL = bmp8bppL / 4;
+        byte[] bbBmp2bpp = new byte[bmp2bppL];
+        for (int i = 0; i < bmp2bppL; i++) for (int j = 3; j >= 0; j--)
+            bbBmp2bpp[i] += cv8bppTo2bpp(bbBmp8bpp[i * 4 + 3 - j], j);
+        //File.WriteAllBytes(fPrefix + fileN.ToString() + ".b8", bbBmp8bpp.ToArray());
+        //File.WriteAllBytes(fPrefix + fileN.ToString() + ".b2", bbBmp2bpp.ToArray());
+
+        // Транспонировать матрицу
+        byte[] bbBmp2bppT = new byte[bmp2bppL];
+        const int bmpW = 80;
+        const int bmpH = 200;
+        for (int x = 0; x < bmpH; x++) for (int y = 0; y < bmpW; y++)
+            bbBmp2bppT[x + y * bmpH] = bbBmp2bpp[y + x * bmpW];
+        //File.WriteAllBytes(fPrefix + fileN.ToString() + ".b2T", bbBmp2bppT.ToArray());
+
+        // Сжать картинку
+        List<byte> resBuf = new List<byte>();
+        resBuf.Add(0x00);
+        resBuf.Add(0x00);
+
+        byte lastByte = 0x00;
+        byte byteCnt = 0x00;
+
+        for (int i = 0; i < bmp2bppL; i++) {
+          if (i > 0) {
+            if (bbBmp2bppT[i] == lastByte) byteCnt++;
+            else {
+              if (byteCnt > 2) {
+                resBuf.Add(0x7B);
+                resBuf.Add(byteCnt);
+                resBuf.Add(lastByte);
+                //lastByte = bbBmp2bppT[i];
+                byteCnt = 0x00;
+              } else if (lastByte == 0x7B) {
+                resBuf.Add(0x7B);
+                resBuf.Add(byteCnt);
+                resBuf.Add(lastByte);
+                byteCnt = 0x00;
+              } else {
+                resBuf.Add(lastByte);
+                for (int j = 0; j < byteCnt; j++) resBuf.Add(lastByte);
+                byteCnt = 0x00;
+              }
+            }
+          }
+          lastByte = bbBmp2bppT[i];
+        }
+
+        if (byteCnt > 2) {
+          resBuf.Add(0x7B);
+          resBuf.Add(byteCnt);
+          resBuf.Add(lastByte);
+          //lastByte = bbBmp2bppT[i];
+          byteCnt = 0x00;
+        } else if (lastByte == 0x7B) {
+          resBuf.Add(0x7B);
+          resBuf.Add(byteCnt);
+          resBuf.Add(lastByte);
+          byteCnt = 0x00;
+        } else {
+          resBuf.Add(lastByte);
+          for (int j = 0; j < byteCnt; j++) resBuf.Add(lastByte);
+          byteCnt = 0x00;
+        }
+
+        resBuf[0] = (byte)(resBuf.Capacity % 0x100);
+        resBuf[1] = (byte)(resBuf.Capacity / 0x100);
+
+        // Сохранить
+        File.WriteAllBytes(fPrefix + fileN.ToString(), resBuf.ToArray());
+      }
+    }
+  }
+}
